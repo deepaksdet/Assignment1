66b71f86b1e8e5cdcf9e8efa134a24887526d83c
DeepakHG <deepakhg15@gmail.com>
2020-12-29 21:24:12 +0530

    New Comments

--- Subscription.py
+++ Subscription.py
@@ -2,26 +2,26 @@ import sys
 try:
     total_amount = 0
 
-    # Using a dictionary for streaming subscription tariff
+# Using a dictionary for streaming subscription tariff
     subscribe_plan = {"prime": 999, "zee5": 999, "sony liv": 999, "netflix": 1947}
 
-    # Taking inputs from the user:
+# Taking inputs from the user:
     print('Please Enter your Budget:')
     budget = input()
     budget_int = int(budget)
 
-    # Check if the budget less than minimum subscription price.
+# Check if the budget less than minimum subscription price.
     if budget_int < 999:
         print("Sorry... No Subscription available for Budget provided Rupees : {}".format(budget))
         print("Note: Minimum subscription starts from 999 or above.")
         sys.exit(1)
 
-    # Taking the input from the user:
+# Taking the input from the user:
     print('Please Select Provider (Prime, zee5, sony liv, Netflix) to subscribe. (For more then one values please use comma ", "):')
     providers = input()
     providers = providers.lower().split(',')
 
-    # Validating users input against the budget:
+# Validating users input against the budget:
     for provider in providers:
 
         amount = subscribe_plan[provider]
